b0903c9467a52a4ccb49902fa12645e2bb6defa0
gleb <gleb@kullect.com>
2011-03-02 23:31:07 -0800

    trying to get a list of songs

--- songs_list_test.php
+++ songs_list_test.php
@@ -1,4 +1,10 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<?php 
+	// Getting list of songs
+	$DB = new SQLQuery();
+	$DB->chooseTable(DB_SONGS_TABLE);
+	$songs = $DB->selectAllOrderBy("'artist' ASC, 'album' ASC, 'title' ASC");
+	var_dump($songs);
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> 
 <head> 
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 

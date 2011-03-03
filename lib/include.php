84ec5c846280fdc3d3a701b40bee985f5c5fec3b
Niels Joubert <njoubert@gmail.com>
2011-03-02 20:58:50 -0800

    twitter workflow

--- lib/include.php
+++ lib/include.php
@@ -4,7 +4,7 @@ define('CONSUMER_SECRET', 'mIr7nEy3RcVnu3QW2e1Hh1KA1ZgMeE2fSbsNhQsSFY');
 define('OAUTH_CALLBACK', 'http://glebden.com/djdp3000/twitter_callback.php');
 define('DB_USER','db8468_autodj');
 
-include 'twitteroauth.php';
+require('twitteroauth.php');
 
 
 ?>

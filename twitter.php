d2ce35e28d1d6e168f3a51dcd759053f26b4414a
Niels Joubert <njoubert@gmail.com>
2011-03-03 01:57:59 -0800

    twitter sql adding user

--- twitter.php
+++ twitter.php
@@ -23,44 +23,41 @@ if (isset($_REQUEST['oauth_token'])) {
 		//if the user exists, update. if not, create.
 		$DB = new SQLQuery();
 		$DB->chooseTable(DB_USERS_TABLE);
-		$DB->toggleDebug();		
-		$user = $DB->selectWhatWhere("*", "tw_id = " . $content->id);
+		$DB->toggleDebug();
 
-		if (empty($user)) {
-			echo "NEW USER";
-			
-			//new user!
-			$dataInsert = array();
-			$fullname = explode(" ", $content->name);
-			$fname = "Britney";
-			$lname = "Spears";
-			if (count($fullname) > 0) {
-				$fname = $fullname[0];
-			}
-			if (count($fullname) > 1) {
-				$lname = $fullname[1];
-			}
-			$dataInsert['tw_id'] = $content->id;
-			$dataInsert['fname'] = $fname;
-			$dataInsert['lname'] = $lname;
-//			$dataInsert['tw_name'] = $screen_name;
-			$dataInsert['avatar'] = $content->profile_image_url;
-			$user = $DB->addItemsArray($dataInsert);
-			if (empty($user)) {
-				echo "GOT BACK EMPTY AFTER SAVE";
-			}
-			
-		} else {
-			//update user!
-			echo "OLD USER";
-		}
-		
+		$dataInsert = array();
+		$fullname = explode(" ", $content->name);
+		$fname = "Britney";
+		$lname = "Spears";
+		if (count($fullname) > 0) { $fname = $fullname[0]; }
+		if (count($fullname) > 1) { $lname = $fullname[1]; }
+		$dataInsert['tw_id'] = $content->id;
+		$dataInsert['fname'] = $fname;
+		$dataInsert['lname'] = $lname;
+		$dataInsert['tw_name'] = $content->screen_name;
+		$dataInsert['avatar'] = $content->profile_image_url;
+		$dataInsert['tw_oauth_token'] = $access_token['oauth_token'];
+		$dataInsert['tw_oauth_token_secret'] = $access_token['oauth_token_secret'];
+
+
+		$user = $DB->selectWhatWhere("*", "tw_id = " . $content->id);
 		print_r($user);
-		
-		$_SESSION['status'] = 'verified';
-		$_SESSION['twitter_uid'] = $content->id;
-		$_SESSION['user_id'] = $user->id;
-		//header('Location: ./index.php');
+		// if (empty($user)) { 
+		// 	$user = $DB->addItemsArray($dataInsert);
+		// } else {
+		// 	$user_id = $user[0]["id"];
+		// 	
+		// 	$user = $DB->updateWhatWhereArray();
+		// }
+		// if (empty($user)) {
+		// 	$_SESSION['status'] = 'error';
+		// 	$_SESSION['twitter_uid'] = $content->id;
+		// 	$_SESSION['user_id'] = $user->id;
+		// } else {
+		// 	$_SESSION['status'] = 'verified';
+		// }
+		// 
+		// header('Location: ./index.php');
 	} else {
 		header('Location: ./clearsessions.php');
 	}
@@ -73,12 +70,10 @@ if (isset($_REQUEST['oauth_token'])) {
 	$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
 	switch ($connection->http_code) {
 		case 200:
-		/* Build authorize URL and redirect user to Twitter. */
 			$url = $connection->getAuthorizeURL($token);
 			header('Location: ' . $url); 
 			break;
 		default:
-	    	/* Show notification if something went wrong. */
 	    	echo 'Could not connect to Twitter. Refresh the page or try again later.';
 	}
 

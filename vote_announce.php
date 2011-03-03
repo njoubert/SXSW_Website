60f50dc2c1e7c541ae163e07afe065cde69d5a97
Niels Joubert <njoubert@gmail.com>
2011-03-02 21:26:21 -0800

    twitter workflow

--- vote_announce.php
+++ vote_announce.php
@@ -5,8 +5,8 @@ require_once('lib/include.php');
 $access_token = $_SESSION['access_token'];
 $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
 
-$connection->post('statuses/update', array('status' => 'I love the twitter api!'));
+$resp = $connection->post('statuses/update', array('status' => 'I love the twitter api!'));
 
-
+print_r($resp)
 
 ?>
